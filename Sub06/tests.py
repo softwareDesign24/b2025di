d75deb6ad94d3a4565eac56f5c88391633de97a7
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2025-05-13 00:07:26 +0300

    Create tests.py

---
+++ Sub06/tests.py
@@ -0,0 +1,13 @@
+# import pytest
+from circulatory_system import Heart
+
+# Starting tests...
+def test_01():
+    a = Heart(60)
+    """ Check that check_health = True """
+    assert a.check_health() == True
+
+def test_02():
+    a = Heart(200)
+    """ Check that check_health = True """
+    assert a.check_health() == False
