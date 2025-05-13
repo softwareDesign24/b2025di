8b470b8062b6f2208059c1b087cd3a57ec254bea
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2025-05-13 08:42:30 +0300

    Update test_01.py

--- Sub06/test_01.py
+++ Sub06/test_01.py
@@ -4,10 +4,10 @@ from circulatory_system import Heart
 # Starting tests...
 def test_01():
     a = Heart(60)
-    """ Check that check_health = True """
+    """ Check that check_health() == True """
     assert a.check_health() == True
 
 def test_02():
     a = Heart(200)
-    """ Check that check_health = False """
+    """ Check that check_health() == False """
     assert a.check_health() == False
